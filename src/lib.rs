fc3bafd27592e49f5e127a67e4446d2ad6c9bcab
Lewis <priorlewis43@gmail.com>
2026-03-30 01:36:42 -0500

    chore: Update beads tracking and src/lib.rs module exports

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,24 @@
 #![allow(dead_code)]
 
 #[cfg(not(target_arch = "wasm32"))]
-pub mod feedback;
-pub mod flow_extender;
-pub mod graph;
-pub mod restate_client;
-pub mod restate_sync;
-
-#[cfg(not(target_arch = "wasm32"))]
 pub mod agent_feedback;
 #[cfg(not(target_arch = "wasm32"))]
 pub mod coverage;
 #[cfg(not(target_arch = "wasm32"))]
 pub mod dashboard;
+pub mod error;
+pub mod expression_depth;
+#[cfg(not(target_arch = "wasm32"))]
+pub mod feedback;
+pub mod flow_extender;
+pub mod graph;
 #[cfg(not(target_arch = "wasm32"))]
 pub mod linter;
 #[cfg(not(target_arch = "wasm32"))]
 pub mod metrics;
+#[cfg(not(target_arch = "wasm32"))]
+pub mod restate_client;
+pub mod restate_sync;
 #[cfg(not(target_arch = "wasm32"))]
 pub mod scenario_runner;
 
@@ -24,6 +26,11 @@ pub mod scenario_runner;
 pub use agent_feedback::{AgentFeedback, FeedbackGenerator};
 #[cfg(not(target_arch = "wasm32"))]
 pub use coverage::{CoverageAnalyzer, CoverageReport};
+pub use error::Error;
+pub use expression_depth::{
+    calculate_depth, resolve_expressions, validate_expression_depth, Expression, ExpressionDepth,
+    ExpressionRegistry, FromExpression, ResolvedExpression, MAX_EXPRESSION_DEPTH,
+};
 #[cfg(not(target_arch = "wasm32"))]
 pub use feedback::{sanitize_results, FeedbackSanitizer};
 pub use graph::{Connection, Node, Viewport, Workflow};
